da8b4df36a0167443e215ef279a9e30b507750d0
Novone2020 <novone2020@gmail.com>
2023-05-03 13:28:17 +0530

    home and course UI only

--- client/src/pages/courses.js
+++ client/src/pages/courses.js
@@ -1,3 +1,59 @@
 export const Courses=()=>{
-    return <div>Courses page</div>
+    return (
+        <div>
+            <section style={{background: "#f5f5f5",paddingTop: "30px",paddingBottom: "5px"}}>
+                <div>
+                    <h1 className="cat">List of Courses</h1>
+                    <center><hr className="hr"/></center>
+                    
+                    <div className="category">
+                        <div className="category-content">
+                            <img src="https://media.istockphoto.com/photos/background-picture-id494343308?k=20&m=494343308&s=612x612&w=0&h=4lteXDK8tCV4j0vAu_VguE59puvINJ6guqNRzGv1PAU="/>
+                            <p>Introduction to C#</p>
+                            <p>Duration: 4 weeks</p>
+                            <a href="#"><button>View Courses</button></a>
+                        </div>
+
+                        <div className="category-content">
+                            <img src="https://images.unsplash.com/photo-1550751827-4bd374c3f58b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHRlY2hub2xvZ3l8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"/>
+                            <p>Data Structure using C++</p>
+                            <p>Duration: 4 weeks</p>
+                            <a href="#"><button>View Courses</button></a>
+                        </div>
+
+                        <div className="category-content">
+                            <img src="https://media.istockphoto.com/vectors/bitcoins-crypto-currency-concept-vector-id1271131500?k=20&m=1271131500&s=612x612&w=0&h=umVZsfa0YwxLs3_JiPMbq9K01XsTe46GK0lGzu_r3m8="/>
+                            <p>Blockchain Technology</p>
+                            <p>Duration: 4 weeks</p>
+                            <a href="#"><button>View Courses</button></a>
+                        </div>
+
+                        <div className="category-content">
+                            <img src="https://media.istockphoto.com/photos/cloud-computing-concept-communication-network-picture-id1277731016?k=20&m=1277731016&s=612x612&w=0&h=xwj4AeLiu3C5HS-IcMV-tr0NXpqJwotlk8-8z-ROV1M="/>
+                            <p>Cloud Computing</p>
+                            <p>Duration: 4 weeks</p>
+                            <a href="#"><button>View Courses</button></a>
+                        </div>
+
+                        <div className="category-content">
+                            <img src="https://media.istockphoto.com/photos/teamwork-with-massive-data-behind-a-futuristic-interface-picture-id1396661329?k=20&m=1396661329&s=612x612&w=0&h=BF3kssMWigVqqPZMeW34SiCpszFQqZ77AYPPqOgM92g="/>
+                            <p>Data Science</p>
+                            <p>Duration: 4 weeks</p>
+                            <a href="#"><button>View Courses</button></a>
+                        </div>
+
+                        <div className="category-content">
+                            <img src="https://media.istockphoto.com/photos/amazon-web-services-aws-advertisement-ad-sign-closeup-in-underground-picture-id925557862?k=20&m=925557862&s=612x612&w=0&h=049gkQW7TOdYc_fHVeAfpIBWYhtalnnfl_pWKb-Glig="/>
+                            <p>AWS Practitioner</p>
+                            <p>Duration: 4 weeks</p>
+                            <a href="#"><button>View Courses</button></a>
+                        </div>
+                    </div>
+
+                </div>
+
+            </section>
+
+        </div>
+    )
 };
--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,3 +1,106 @@
 export const Home=()=>{
-    return <div>Home page</div>
+    return(
+    <div className="homePage">
+
+        {/* Category */}
+        <section style={{background:"#f5f5f5",paddingTop: "30px",paddingBottom: "5px"}}>
+            <div>
+                <h1 className="cat">Course Catagory</h1>
+                <center><span><hr className="hr"/></span></center>
+                
+                <div className="category">
+                    <div className="category-content">
+                        <img src="https://images.unsplash.com/photo-1607706189992-eae578626c86?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y29tcHV0ZXIlMjBlbmdpbmVlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"/>
+                        <p>Computer Engineering</p>
+                        <a href="#"><button>View Courses</button></a>
+                    </div>
+
+                    <div className="category-content">
+                        <img src="https://images.unsplash.com/photo-1550751827-4bd374c3f58b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHRlY2hub2xvZ3l8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"/>
+                        <p>Information Technology</p>
+                        <a href="#"><button>View Courses</button></a>
+                    </div>
+
+                    <div className="category-content">
+                        <img src="https://images.unsplash.com/photo-1518770660439-4636190af475?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZWxlY3Ryb25pY3N8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"/>
+                        <p>Electronics and Telecommunication</p>
+                        <a href="#"><button>View Courses</button></a>
+                    </div>
+
+                    <div className="category-content">
+                        <img src="https://images.unsplash.com/photo-1619765617659-f3dcb700c27f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fG1lY2hhbmljYWwlMjBlbmdpbmVlcmluZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"/>
+                        <p>Mechanical Engineering</p>
+                        <a href="#"><button>View Courses</button></a>
+                    </div>
+
+                    <div className="category-content">
+                        <img src="https://images.unsplash.com/photo-1625722662233-297060231b85?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Y2l2aWwlMjBlbmdpbmVlcmluZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"/>
+                        <p>Civil Engineering</p>
+                        <a href="#"><button>View Courses</button></a>
+                    </div>
+
+                    <div className="category-content">
+                        <img src="https://plus.unsplash.com/premium_photo-1661877130397-0ed4174532a5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8YXV0b21vYmlsZSUyMGVuZ2luZWVyaW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"/>
+                        <p>Automobile Engineering</p>
+                        <a href="#"><button>View Courses</button></a>
+                    </div>
+                </div>
+
+            </div>
+
+        </section>
+        {/* Category */}
+        
+        {/* Testimonial */}
+        <section >
+            <h1 style={{marginTop: "40px"}}>Testimonials</h1>
+            <center><span><hr className="hr" style={{marginBottom: "70px"}}/></span></center>
+            <div style={{background: "#0082e6"}}>
+            <div className="testimonials">
+                <div className="test-data">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid laboriosam eos porro, fugit ex, eaque aspernatur est deserunt odit iure sapiente dicta dolor facere nisi cumque praesentium fugiat corporis! Dignissimos.</p>
+                </div>
+                <div className="test-data">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid laboriosam eos porro, fugit ex, eaque aspernatur est deserunt odit iure sapiente dicta dolor facere nisi cumque praesentium fugiat corporis! Dignissimos.</p>
+                </div>
+                <div className="test-data">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid laboriosam eos porro, fugit ex, eaque aspernatur est deserunt odit iure sapiente dicta dolor facere nisi cumque praesentium fugiat corporis! Dignissimos.</p>
+                </div>
+            </div>
+        </div>
+        </section>
+        {/* Testimonial */}
+
+        {/* Footer */}
+        <section style={{background: "#2c3e50"}}>
+            <div className="footer">
+                <div className="footer-content">
+                    <div className="icons">
+                        <div> 
+                            <a href="#">Facebook</a>
+                        </div>  
+                        
+                        <div>
+                            <a href="#">Instagram</a>
+                        </div>
+
+                        <div>
+                            <a href="#">Twitter</a>
+                        </div>
+
+                        <div> 
+                            <a href="#">Linked IN</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="footer-content">
+                    <h2>About US </h2><br/><br/>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam sapiente, at libero quae, non delectus enim dolor eum error molestias repellat maiores ullam. Voluptatum ipsa porro officia et ducimus nihil ullam maxime, mollitia similique omnis optio quia soluta neque, sed vel officiis minus alias provident eius excepturi blanditiis facere! Ex.</p>
+                </div>
+            </div>
+        </section>
+        {/* Footer */}
+
+    </div>
+    )
 };
